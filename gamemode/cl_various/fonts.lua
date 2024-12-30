13bea3cf2d0050186b82e68f71eb4026a39bd525
themaw <cjbremer@gmail.com>
2024-12-30 23:45:49 +0100

    Added scoreboard font for later

--- gamemode/cl_various/fonts.lua
+++ gamemode/cl_various/fonts.lua
@@ -10,3 +10,5 @@ surface.CreateFont( "c13_normal", {size = 18, weight = 0, antialias = false, fon
 
 surface.CreateFont( "c13_chatfont", {size = 22, antialias = false, font = "Roboto-Regular"} )
 surface.CreateFont( "c13_chatfont_outlined", {size = 22, antialias = false, font = "Roboto-Regular", outline = true} )
+
+surface.CreateFont( "c13_scoreboard", {size = 32, weight = 0, antialias = false, font = "Roboto-Bold"} )
